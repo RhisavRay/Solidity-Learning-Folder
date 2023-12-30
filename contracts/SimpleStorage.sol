0d05815db2cfbcb68149f1d6f1982aecef6a845e
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-12-30 23:07:31 +0530

    Comments added for ease of understanding

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -7,12 +7,22 @@ contract SimpleStorage //This is how you declare a contract
     // bool hasFavouriteNumber = true; // or 'false'
     // string favouriteNumber = "Five";
     // address myAddress = 0x2b894F6fd066ee7A2f8af73D5668d897AC855690;
-    uint256 public favouriteNumber; //If not explicitly mentioned, the value stored is the null value, which is zero by default
+    
+    uint256 public favouriteNumber;
+    // If not explicitly mentioned, the value stored is the null value, which is zero by default
     // The public key word above means that now the variable declared has visibility set to public
     // By default visibility of anything is set to private, hence the public keyword after the function declaration as well
+    // Also, this variable has global range, that is everything within this contract can access this access it freely, exactly similar to how variable scope works in JS
     
     function store(uint256 _favouriteNumber) public
     {
         favouriteNumber = _favouriteNumber;
+    }   
+
+    function retrieve() public view returns(uint256)
+    {
+        return favouriteNumber;
     }
+    // 'view' or 'pure' mode of function disallows making updates to data in the blockchain
+    // This mode doesn't cost gas, except if it is called by a gas consuming function
 }
