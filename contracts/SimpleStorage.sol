422bf4387cf62831a6c2f947fc9e250bd6cab127
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-12-30 22:01:07 +0530

    Deployed my first contract, with a simple function declaration

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7; //The ^{version} means I am asking it to use the version mentioned, or a more latest one
+
+contract SimpleStorage //This is how you declare a contract
+{
+    // Basic data types -> boolean, uint(unsigned int), int, address(account address), bytes
+    // bool hasFavouriteNumber = true; // or 'false'
+    // string favouriteNumber = "Five";
+    // address myAddress = 0x2b894F6fd066ee7A2f8af73D5668d897AC855690;
+    uint256 public favouriteNumber; //If not explicitly mentioned, the value stored is the null value, which is zero by default
+    // The public key word above means that now the variable declared has visibility set to public
+    // By default visibility of anything is set to private, hence the public keyword after the function declaration as well
+    
+    function store(uint256 _favouriteNumber) public
+    {
+        favouriteNumber = _favouriteNumber;
+    }
+}
